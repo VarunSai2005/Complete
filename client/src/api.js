0881384cbbc36eb910740c65fbbdef57bb5243db
VarunSai2005 <varunsaipasupuleti2005@gmai.com>
2025-11-04 19:42:04 +0530

    Updated files

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const BASE_URL = 'https://noque-server.onrender.com/' || 'http://localhost:5000';
 
 export const api = axios.create({
   baseURL: BASE_URL + '/api',
